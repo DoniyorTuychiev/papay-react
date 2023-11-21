7b791f5a9af4c1cea5f2e49af043815e80ff1bea
doni <doni0101195@gmail.com>
2023-11-21 20:01:28 +0900

    :feat: develop nested routing for restaurant screen

--- src/app/screens/RestaurantPage/index.tsx
+++ src/app/screens/RestaurantPage/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Container } from "@mui/material";
+import { Switch, useRouteMatch, Route} from 'react-router-dom';
+import { ChoosenDish } from './choosenDish';
+import { OneRestaurant } from './oneRestaurant';
+import { AllRestaurants } from './allRestaurants';
 
 export function RestaurantPage() {
+    let restaurant = useRouteMatch();
+    console.log(restaurant);
     return (
-        <Container>RestaurantPage</Container>
+        <div className = 'restaurant_page'>
+            <Switch>
+                <Route path = {`${restaurant.path}/dish/:dish_id`}>
+                    <ChoosenDish/>
+                    </Route>
+                <Route path = {`${restaurant.path}/:restaurant_id`}>
+                    <OneRestaurant/>
+                    </Route>
+                <Route path = {`${restaurant.path}`}>
+                    <AllRestaurants/>
+                    </Route>
+            </Switch>
+        </div>
     );
 }
 
